ac3c4e202385284e71427da24f88cf88020d028a
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-10 09:24:00 +0300

    fix: app

--- Modulse/Node-Module/app.js
+++ Modulse/Node-Module/app.js
@@ -1,3 +1,14 @@
 const user = require('./user.js');
 
 console.log(user)
+
+
+
+// Асель@LAPTOP-******** ******* ~/OneDrive/Desktop/my_saite.html/КЕЛКЧЕК--2022/Paterns_Modules_2022_KK/Modulse/Node-Module (moduls)
+// $ node app.js
+// {
+//   jone: { name: 'Jone', age: 22 },
+//   pete: { name: 'Pete', age: 20 },
+//   bob: { name: 'Bob', age: 27 },
+//   logUser: [Function: logUser]
+// } 
