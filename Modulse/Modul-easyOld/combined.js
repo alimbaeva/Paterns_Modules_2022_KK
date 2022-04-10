c5c02bf6aaba8028f6709f69826593e76c98de61
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-10 08:39:16 +0300

    feat: modul for old

---
+++ Modulse/Modul-easyOld/combined.js
@@ -0,0 +1,28 @@
+const jone = {
+    name: "Jone",
+    age: 22
+};
+
+const pete = {
+    name: "Pete",
+    age: 20
+};
+
+
+bob = {
+    name: "Bob",
+    age: 27
+};
+
+
+function logUser(user) {
+    console.log(`My name is ${user.name}. I am ${user.age} age`)
+}
+
+
+console.log(jone)
+console.log(petya)
+console.log(bob)
+
+logUser(jone)
+logUser(bob)
