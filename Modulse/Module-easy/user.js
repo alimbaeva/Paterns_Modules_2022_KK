3656de246de7844037f7e9f2797de1a16367e322
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-10 08:26:04 +0300

    feat: modul

---
+++ Modulse/Module-easy/user.js
@@ -0,0 +1,21 @@
+export const jone = {
+    name: "Jone",
+    age: 22
+};
+
+export const pete = {
+    name: "Pete",
+    age: 20
+};
+
+
+const bob = {
+    name: "Bob",
+    age: 27
+};
+
+
+export function logUser(user) {
+    console.log(`My name is ${user.name}. I am ${user.age} age`)
+}
+export default bob;
